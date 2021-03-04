d85cb39b7a5efc3cd4dd4b1e54abb676293f17c7
Vlad <vladlungu2121@gmail.com>
2021-03-04 19:13:48 +0200

    Seminar 2

---
+++ src/ro/ase/cts/clase/readers/IReader.java
@@ -0,0 +1,17 @@
+package ro.ase.cts.clase.readers;
+
+import ro.ase.cts.clase.Aplicant;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public abstract class IReader {
+    protected String filePath;
+
+    public IReader(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public abstract List<Aplicant> readAplicants() throws FileNotFoundException ;
+
+}
---
+++ src/ro/ase/cts/program/Program.java
@@ -0,0 +1,29 @@
+package ro.ase.cts.program;
+
+import ro.ase.cts.clase.Angajat;
+import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.clase.readers.AngajatReader;
+import ro.ase.cts.clase.readers.IReader;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public class Program {
+
+	public static List<Aplicant> citesteAplicanti(IReader readAplicant) throws FileNotFoundException{
+		return readAplicant.readAplicants();
+	}
+
+	public static void main(String[] args) {
+		List<Aplicant> listaAplicanti;
+		try {
+			listaAplicanti = citesteAplicanti(new AngajatReader("angajati.txt"));
+			for(Aplicant aplicant:listaAplicanti)
+				System.out.println(aplicant.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
